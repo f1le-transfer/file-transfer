5dad659cef8629b7d98da4e6e155f54d431b0982
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-03 10:52:07 +0300

    parse data from post request

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const app = require('express')()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.send('hi')
+})
+
+app.post('/exec', (req, res) => {
+  let {body: data} = req
+  
 })
 
 app.listen(8080)
