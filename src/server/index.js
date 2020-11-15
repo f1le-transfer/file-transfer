e2dc76c5e94a805b84fe71b205f5aa032dc330eb
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-15 04:26:08 +0300

    config

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
+require('dotenv').config({ path: path.join(__dirname, 'config', '.env') })
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.listen(80)
+app.listen(process.env.PORT)
