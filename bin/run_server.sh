9c814e94260c59f58cbf4863ca5f0943b166c39b
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-15 03:41:17 +0300

    script exit status

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,6 +1,6 @@
 path=./src/server/index.js
 
-if [ $1 == 'dev' ]
+if [[ $1 == 'dev' ]]
 then
   echo '**Server is running in development mode**'
   echo
@@ -9,4 +9,10 @@ else
   echo '**Server is running in production mode**'
   echo
   node $path
+fi
+
+if [ $? -eq 130 ]; then 
+  exit 0
+else 
+  exit $?
 fi
