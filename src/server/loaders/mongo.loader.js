1aa043c9de3e7f36ff092a32ac2f9ffbe96b9d23
loveyousomuch <loveyousomuch554@gmail.com>
2020-12-19 23:09:53 +0300

    connect to mongodb

--- src/server/loaders/mongo.loader.js
+++ src/server/loaders/mongo.loader.js
@@ -2,4 +2,4 @@ const { MongoClient } = require('mongodb')
 
 const client = new MongoClient(process.env.MongoDB, { useUnifiedTopology: true })
 
-client.connect().then(console.log)
+client.connect().catch(console.log)
