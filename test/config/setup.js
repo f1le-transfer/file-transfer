9bdb203d82be992580629dad0433aa2e2cdaaa35
loveyousomuch <loveyousomuch554@gmail.com>
2020-12-26 23:52:30 +0300

    change env var to test db

--- test/config/setup.js
+++ test/config/setup.js
@@ -2,5 +2,6 @@ require('dotenv').config({ path:  `${__dirname}/../../config/.env'`})
 
 module.exports = async function() {
   require('../../build/server/server')
+  process.env.db = 'test'
   console.log("\nSetup Mongo Connection")
 }
