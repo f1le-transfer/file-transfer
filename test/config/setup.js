10e73f974c39f3570d3cf54eeb30933407c542b0
loveyousomuch <loveyousomuch554@gmail.com>
2020-12-31 01:53:49 +0300

    Renaming env variables

--- test/config/setup.js
+++ test/config/setup.js
@@ -2,6 +2,12 @@ require('dotenv').config({ path:  `${__dirname}/../../config/.env'`})
 
 module.exports = async function() {
   require('../../build/server/server')
-  process.env.db = 'test'
+
+  /*
+  * Overwritten the variable because this
+  * variable is used to connect to collections(users, sessions) in src/server/dao/users.dao.js
+  * */
+  process.env.db_name = process.env.db_name_test
+
   console.log("\nSetup Mongo Connection\n")
 }
