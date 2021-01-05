ebfc6fbfff59c5db680856f52573fdb07c40230a
loveyousomuch <loveyousomuch554@gmail.com>
2021-01-05 22:17:46 +0300

    users route

---
+++ src/server/routes/users.route.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import UserCtrl from '../controllers/users.controller'
+const _Router = Router()
+
+_Router.post('/register', UserCtrl.register)
+_Router.post('/login', UserCtrl.login)
+_Router.post('/logout', UserCtrl.logout)
+_Router.delete('/delete', UserCtrl.delete)
+
+export default _Router
