ca670353688409220549fc14bef946e02b3550ce
loveyousomuch <loveyousomuch554@gmail.com>
2021-01-08 18:31:15 +0300

    TrackAFK users

--- src/server/loaders/mongo.loader.js
+++ src/server/loaders/mongo.loader.js
@@ -1,10 +1,12 @@
 import { MongoClient } from 'mongodb';
 import UsersDAO from '../dao/users.dao';
+import TrackAFK from '../subscribers/trackAFK.sub';
 
 const client = new MongoClient(process.env.db_uri, { useUnifiedTopology: true })
 
 client.connect()
  .then( async (client) => {
    await UsersDAO.injectDB(client)
+   await TrackAFK.injectDB(client)
  })
  .catch(console.log)
