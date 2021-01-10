81dfa21c3c939a6d6fa88b6e3d2f993d4b499583
loveyousomuch <loveyousomuch554@gmail.com>
2021-01-10 18:01:49 +0300

    TrackAFK_DAO & color log

--- src/server/loaders/mongo.loader.js
+++ src/server/loaders/mongo.loader.js
@@ -1,12 +1,15 @@
 import { MongoClient } from 'mongodb';
 import UsersDAO from '../dao/users.dao';
-import TrackAFK from '../subscribers/trackAFK.sub';
+import TrackAFK_DAO from '../dao/TrackAFK.dao';
 
 const client = new MongoClient(process.env.db_uri, { useUnifiedTopology: true })
 
 client.connect()
  .then( async (client) => {
+   console.log('\x1b[35m%s\x1b[0m', '\nConnecting to database...');
+
    await UsersDAO.injectDB(client)
-   await TrackAFK.injectDB(client)
+   await TrackAFK_DAO.injectDB(client)
+   console.log('\x1b[35m%s\x1b[0m', `The server is running on port ${process.env.PORT}\n`)
  })
  .catch(console.log)
