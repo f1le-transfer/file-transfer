c92ffa8272c42b57c1a0de22709f17aba57c22c0
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-23 16:56:19 +0300

    http server

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,6 @@
 import app from './index';
 import fs from 'fs';
 import https from 'https';
-import http from 'http';
 import path from 'path';
 
 const prvtKey = fs.readFileSync(path.join(process.env.PWD, 'config', 'example.com+5-key.pem'), 'utf-8')
@@ -13,11 +12,10 @@ const credentials = {
 }
 
 const httpsServer = https.createServer(credentials, app)
-const httpServer = http.createServer(app);
 
 // Connect mongoDB and run app (express server)
 import('./loaders/mongo.loader').then(() => { 
     httpsServer.listen(process.env.PORT); /*app.listen(process.env.PORT)*/
-    httpServer.listen(process.env.HTTP_PORT);
+    app.listen(process.env.HTTP_PORT);
   }
 )
