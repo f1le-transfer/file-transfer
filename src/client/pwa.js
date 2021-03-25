82eacbe437c164bdd3b1fbffdba4d1c1b68c20c8
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-25 22:02:12 +0300

    test another protocol

--- src/client/pwa.js
+++ src/client/pwa.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', init, false);
-const log = (info, color='green') => console.log(`%c${info}`, `color: ${color};`)
+const log = (info) => console.log(`%c[Service Worker] ${info}`, `color: green;`)
 
 function init() {
   if ('serviceWorker' in navigator && navigator.onLine) {
     navigator.serviceWorker.register('/sw.js')
-      .then(reg => log('[Service Worker sw.js] Registered', reg))
-      .catch(err => console.error('[Service Worker sw.js] Not registered ->', err))
+      .then(reg => log('Registered', reg))
+      .catch(err => console.error('Not registered ->', err))
     
-    // navigator.serviceWorker.register('/tcp_conn.js')
-    //   .then(reg => log('[Service Worker] tcp_conn.js worker registered', reg))
-    //   .catch(err => console.error('[Service Worker tcp_conn.js] Not registered ->', err))
+    navigator.serviceWorker.register('/tcp_conn.js')
+      .then(reg => log('Registered', reg))
+      .catch(err => console.error('Not registered ->', err))
   }
 }
--- src/client/tcp_conn.js
+++ src/client/tcp_conn.js
@@ -1,44 +1,5 @@
-const HEADER = 256
-const BUF_SIZE = 4096
-const PORT = 5050
-const HOST = '127.0.0.1'
+const broadcast = new BroadcastChannel('tcp_channel')
 
-/**
- * To communicate between main window and worker we will use the BroadcastChannel.
- */
-
-// Channel for communicate between window and worker
-const broadcast = new BroadcastChannel('tcp_channel'); 
-
-const connect = () => new WebSocket(`ws://${HOST}:${PORT}`)
-const close = () => socket.close(1000)
-
-const socket = new WebSocket('wss://127.0.0.1:5050')
-
-function encode_utf8(s) {
-  return unescape(encodeURIComponent(s));
-}
-
-function decode_utf8(s) {
-  return decodeURIComponent(escape(s));
-}
-
-socket.addEventListener('open', () => {
-  console.log('[TCP] Connection open')
-});
-
-// listen to messages
 broadcast.addEventListener('message', ({ data }) => {
-  if (data) {
-    console.log('[tcp_conn Service Worker] ⬇')
-    console.log(data)
-  }
+  console.log(data)
 })
-
-const send_msg_window = msg => broadcast.postMessage(msg)
-
-function set_header(file_name, req_type='POST') {
-  const header = `TCP ${req_type}\nChunk-name: ${file_name}.chunk\nContent-length: 0\n`
-
-  return header += ' '.repeat(HEADER-header.length)
-}
