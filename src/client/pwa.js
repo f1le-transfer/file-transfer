ea8f8c0420f67bef1aca6ed9d94e0f11838f443e
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-25 21:13:47 +0300

    something like tcp

--- src/client/pwa.js
+++ src/client/pwa.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', init, false);
+const log = (info, color='green') => console.log(`%c${info}`, `color: ${color};`)
 
 function init() {
   if ('serviceWorker' in navigator && navigator.onLine) {
     navigator.serviceWorker.register('/sw.js')
-      .then((reg) => {
-        console.log('[Service Worker] Registered ->', reg);
-      }, (err) => {
-        console.error('[Service Worker] Not registered ->', err);
-      });
+      .then(reg => log('[Service Worker sw.js] Registered', reg))
+      .catch(err => console.error('[Service Worker sw.js] Not registered ->', err))
+    
+    // navigator.serviceWorker.register('/tcp_conn.js')
+    //   .then(reg => log('[Service Worker] tcp_conn.js worker registered', reg))
+    //   .catch(err => console.error('[Service Worker tcp_conn.js] Not registered ->', err))
   }
 }
