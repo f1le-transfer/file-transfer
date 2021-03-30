f4a2c7a4d866edb6736d6eb41d65ff6bbc081a78
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-30 11:42:28 +0300

    send message to the tcp server

--- src/client/tcp_conn.js
+++ src/client/tcp_conn.js
@@ -1,5 +1,23 @@
+const socket = new WebSocket("ws://127.0.0.1:5050");
+
 const broadcast = new BroadcastChannel('tcp_channel')
 
 broadcast.addEventListener('message', ({ data }) => {
-  console.log(data)
+  socket.send(JSON.stringify(data))
 })
+
+socket.onopen = function(e) {
+  console.log("[TCP open] Connection open");
+}
+
+socket.onmessage = function(event) {
+  console.log(`[TCP msg] ${event.data}`)
+}
+
+socket.onclose = function(event) {
+  console.log(`[TCP close] Code: ${event.code} ${event.reason}`)
+}
+
+socket.onerror = function(error) {
+  console.log(`[TCP error] ${error.message}`);
+}
