d08928da3d5514e4a4e4358cde3bc9775479d14e
loveyousomuch <loveyousomuch554@gmail.com>
2021-04-01 23:51:48 +0300

    connect by command

--- src/client/tcp_conn.js
+++ src/client/tcp_conn.js
@@ -1,23 +1,30 @@
-const socket = new WebSocket("ws://127.0.0.1:5050")
+let socket;
 const broadcast = new BroadcastChannel('tcp_channel')
 
 broadcast.addEventListener('message', ({ data }) => {
-  socket.send(JSON.stringify(data))
+  if (data.msg === 'connect') {
+    socket = set_socket(new WebSocket("ws://127.0.0.1:5050"))
+  } else {
+    socket.send(JSON.stringify(data))
+  }
 })
 
-socket.onopen = function(e) {
-  console.log("[TCP open] Connection open");
-}
-
-socket.onmessage = function(event) {
-  console.log(`[TCP msg] ${event.data}`)
-}
-
-socket.onclose = function(event) {
-  console.log(`[TCP close] Code: ${event.code} ${event.reason}`)
-}
-
-socket.onerror = function(error) {
-  console.log(`[TCP error] ⬇️`);
-  console.log(error)
+function set_socket(socket) {
+  socket.onopen = function(e) {
+    console.log("[TCP open] Connection open");
+  }
+  
+  socket.onmessage = function(event) {
+    console.log(`[TCP msg] ${event.data}`)
+  }
+  
+  socket.onclose = function(event) {
+    console.log(`[TCP close] Code: ${event.code} ${event.reason}`)
+  }
+  
+  socket.onerror = function(error) {
+    console.log(`[TCP error] ⬇️`);
+    console.log(error)
+  }
+  return socket
 }
