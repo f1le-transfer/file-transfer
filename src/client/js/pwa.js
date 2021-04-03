cdafbffd4cc3dc7c1a34db0eba6e2d655e5ca85d
loveyousomuch <loveyousomuch554@gmail.com>
2021-04-03 16:46:37 +0300

    log

--- src/client/js/pwa.js
+++ src/client/js/pwa.js
@@ -4,11 +4,11 @@ const log = (info) => console.log(`%c[Service Worker] ${info}`, `color: green;`)
 function init() {
   if ('serviceWorker' in navigator && navigator.onLine) {
     navigator.serviceWorker.register('/sw.js')
-      .then(reg => log('Registered', reg))
+      .then(reg => log('Registered 1', reg))
       .catch(err => console.error('Not registered ->', err))
     
-    navigator.serviceWorker.register('js/tcp_conn.js')
-      .then(reg => log('Registered', reg))
+    navigator.serviceWorker.register('/tcp_conn.js')
+      .then(reg => log('Registered 2', reg))
       .catch(err => console.error('Not registered ->', err))
   }
 }
