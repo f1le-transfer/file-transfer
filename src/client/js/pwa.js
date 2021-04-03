5ce598630fa356aa1c9d9611b3a673d7d3a9b0f6
loveyousomuch <loveyousomuch554@gmail.com>
2021-04-03 18:14:39 +0300

    rename log func

--- src/client/js/pwa.js
+++ src/client/js/pwa.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', init, false);
-const log = (info) => console.log(`%c[Service Worker] ${info}`, `color: green;`)
+const pwa_log = (info) => console.log(`%c[Service Worker] ${info}`, `color: green;`)
 
 function init() {
   if ('serviceWorker' in navigator && navigator.onLine) {
     navigator.serviceWorker.register('/sw.js')
-      .then(reg => log('Registered 1', reg))
+      .then(reg => pwa_log('Registered 1', reg))
       .catch(err => console.error('Not registered ->', err))
     
     navigator.serviceWorker.register('/tcp_conn.js')
-      .then(reg => log('Registered 2', reg))
+      .then(reg => pwa_log('Registered 2', reg))
       .catch(err => console.error('Not registered ->', err))
   }
 }
