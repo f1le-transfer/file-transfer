c509938c6b2d494a04cb583dd6f328035b0e15de
loveyousomuch <loveyousomuch554@gmail.com>
2021-05-17 14:00:32 +0300

    get available files list

--- src/client/js/pwa.js
+++ src/client/js/pwa.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', init, false);
 const pwa_log = (info) => console.log(`%c[Service Worker] ${info}`, `color: green;`)
+let CURRENT_FILES;
 
 function init() {
   if ('serviceWorker' in navigator && navigator.onLine) {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => pwa_log('Registered 1', reg))
-      .catch(err => console.error('Not registered ->', err))
-    
-    navigator.serviceWorker.register('/tcp_conn.js')
-      .then(reg => pwa_log('Registered 2', reg))
+    Promise.all([navigator.serviceWorker.register('/sw.js'), navigator.serviceWorker.register('/tcp_conn.js')])
+      .then(values => values.forEach((v, i) => pwa_log(`Registered ${i+1}`, v)))
+      .then(getFilesList)
       .catch(err => console.error('Not registered ->', err))
   }
+}
+
+function getFilesList() {
+  const broadcast = new BroadcastChannel('tcp_channel')
+  broadcast.addEventListener('message', ({ data }) => {
+    if (!data.listAvailableFiles) return;
+    data.listAvailableFiles.forEach(file => {
+      document.getElementById('files').innerHTML += (`<p>${file}</p>`)
+    })
+    CURRENT_FILES = data.listAvailableFiles
+  })
+
+  broadcast.postMessage({ getListAvailableFiles: true })
 }
