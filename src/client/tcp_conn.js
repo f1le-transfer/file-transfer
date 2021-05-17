706bf35f0562036321d19e38c1cb23362e50c6b2
loveyousomuch <loveyousomuch554@gmail.com>
2021-05-17 14:01:02 +0300

    await ready state of socket

--- src/client/tcp_conn.js
+++ src/client/tcp_conn.js
@@ -2,7 +2,16 @@ let socket = set_socket(new WebSocket("ws://127.0.0.1:5050"))
 const broadcast = new BroadcastChannel('tcp_channel')
 
 broadcast.addEventListener('message', ({ data }) => {
-  socket.send(JSON.stringify(data))
+  if (socket.readyState === 1) {
+    socket.send(JSON.stringify(data))
+  } else {
+    new Promise((res, rej) => {
+      socket.onopen = () => {
+        socket.send(JSON.stringify(data))
+        res()
+      }
+    })
+  }
 })
 
 function set_socket(socket) {
