9856dc90537558e3dbfd95f98fdfd9a86acf2a8d
loveyousomuch <loveyousomuch554@gmail.com>
2021-05-21 11:17:16 +0300

    connect files

--- src/server/loaders/mongo.loader.js
+++ src/server/loaders/mongo.loader.js
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 import UsersDAO from '../dao/users.dao';
 import TrackAFK_DAO from '../dao/TrackAFK.dao';
+import FilesDAO from '../dao/files.dao';
 
 const {
   MONDO_PROTOCOL,
@@ -37,6 +38,7 @@ client.connect()
 
    await UsersDAO.injectDB(client)
    await TrackAFK_DAO.injectDB(client)
+   await FilesDAO.injectDB(client)
    console.log('\x1b[35m%s\x1b[0m', `The https server is running on port ${process.env.PORT} and http ${process.env.HTTP_PORT}\n`)
  })
  .catch(console.log)
