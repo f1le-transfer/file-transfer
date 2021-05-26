1626dca29b0556278bd3406bbe567e6a9514cd70
loveyousomuch <loveyousomuch554@gmail.com>
2021-05-26 12:13:26 +0300

    compare hours now

--- src/client/js/compare_token.js
+++ src/client/js/compare_token.js
@@ -1,15 +1,13 @@
 function compare_token() {
   const token = JSON.parse(localStorage.getItem('token'))
 
-  function date_diff_in_days(date) {
-    const utc = (d) => {
-      d = new Date(d)
-      return Date.UTC(d.getFullYear(), d.getMonth(), d.getDate())
-    }
-    return Math.floor((utc(new Date().getTime()) - utc(date)) / (1000 * 60 * 60 * 24))
+  function date_diff_in_hours(date) {
+    let diff =(new Date().getTime() - new Date(date).getTime()) / 1000
+    diff /= (60 * 60)
+    return Math.abs(Math.round(diff))
   }
   
-  if (!localStorage.getItem('token') || date_diff_in_days(token.date) > 4) {
+  if (!localStorage.getItem('token') || date_diff_in_hours(token.date) > 4) {
     alert('You need login.')
     window.location.replace('/users/auth')
   }
